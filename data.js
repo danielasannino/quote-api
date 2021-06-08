4e710200050756960f76b61dad4598863d6e26d0
Daniela Sannino <daniela.sannino1@gmail.com>
2021-06-08 17:23:54 -0400

    added GET and POST request functionality

--- data.js
+++ data.js
@@ -12,7 +12,7 @@ const quotes = [
     person: 'Grace Hopper'
   },
   {
-    quote: 'The city’s central computer told you?  R2D2, you know better than to trust a strange computer!',
+    quote: 'The cityâ€™s central computer told you?  R2D2, you know better than to trust a strange computer!',
     person: 'C-3PO'
   },
   {
@@ -32,7 +32,7 @@ const quotes = [
     person: 'Grace Hopper'
   },
   {
-    quote: 'As soon as we started programming, we found to our surprise that it wasn’t as easy to get programs right as we had thought.  Debugging had to be discovered.  I can remember the exact instant when I realized that a large part of my life from then on was going to be spent in finding mistakes in my own programs.',
+    quote: 'As soon as we started programming, we found to our surprise that it wasnâ€™t as easy to get programs right as we had thought.  Debugging had to be discovered.  I can remember the exact instant when I realized that a large part of my life from then on was going to be spent in finding mistakes in my own programs.',
     person: 'Maurice Wilkes'
   },
   {
--- server.js
+++ server.js
@@ -2,9 +2,46 @@ const express = require('express');
 const app = express();
 
 const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
+const { getRandomElement,
+    createElement,
+    getIndexById,
+    updateElement
+} = require('./utils');
 
 const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
+
+//GET random quote
+app.get('/api/quotes/random', (req, res, next) => {
+    const randomQuote = getRandomElement(quotes);
+    res.status(200).send({ quote: randomQuote });
+});
+
+//GET all quotes from one author
+
+app.get('/api/quotes', (req, res, next) => {
+    const quoteSearch = req.query.person;
+    if (quoteSearch) {
+        const authorFilter = quotes.filter(quote => quote.person.toLowerCase() === quoteSearch.toLowerCase());
+        res.status(200).send({ quotes: authorFilter });
+    } else {
+        res.status(200).send({ quotes: quotes })
+    }
+});
+
+//POST new quote
+
+app.post('/api/quotes', (req, res, next) => {
+    const newQuote = { quote: req.query.quote, person: req.query.person }
+    if (newQuote.quote && newQuote.person) {
+        quotes.push(newQuote)
+        res.status(201).send({ quote: newQuote });
+    } else {
+        res.status(404).send();
+    }
+});
